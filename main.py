a185131db44870175214a1d1a6d2f768c7e5bb3c
Katarzyna Michalska <kate.michalska7@gmail.com>
2023-10-20 22:22:31 +0200

    simply responses

--- main.py
+++ main.py
@@ -1,16 +1,39 @@
-# This is a sample Python script.
+import nltk
+from nltk.chat.util import Chat, reflections
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# Przykładowe reguły i odpowiedzi chatbota
+pairs = [
+    [
+        r"witaj|cześć|hej",
+        ["Cześć!"]
+    ],
+    [
+        r"jak się masz?",
+        ["Dobrze, dziękuję! Jak mogę ci dzisiaj pomóc?"]
+    ],
+    [
+        r"co możesz zrobić",
+        ["Jestem prostym chatbotem i mogę odpowiadać na twoje pytania. Spróbuj zapytać o coś!"]
+    ],
+    [
+        r"zamówienie|kupić|produkt",
+        ["Przepraszam, ale jestem tylko chatbotem i nie mogę dokonywać zakupów. Jednak mogę ci doradzić w wyborze produktu."]
+    ],
+    [
+        r"jak mogę cię kontaktować",
+        ["Jestem dostępny tutaj przez ten chat. Jak mogę ci pomóc?"]
+    ],
+    [
+        r"do widzenia",
+        ["Do widzenia! Miłego dnia!"]
+    ],
+]
 
+# Ustawienia rozpoznawania
+def chatbot():
+    print("Witaj! Jestem twój chatbot. Pytaj mnie o cokolwiek, a postaram się odpowiedzieć.")
+    chat = Chat(pairs, reflections)
+    chat.converse()
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+if __name__ == "__main__":
+    chatbot()
