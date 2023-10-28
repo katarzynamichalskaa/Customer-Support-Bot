6f793bc9f73c665e128bf39a1154dc7ce8082504
Katarzyna Michalska <kate.michalska7@gmail.com>
2023-10-28 18:39:34 +0200

    ChatBot functionality

--- Responses.py
+++ Responses.py
@@ -1,7 +1,17 @@
 import nltk
 from nltk.chat.util import Chat, reflections
+from CheckOpinion import initRequest
+from CheckOpinion import LowestPrice
+from CheckOpinion import Find
+from CheckOpinion import BestRated
+import threading
 
 pairs = [
+    [
+        r"Sprawdź (.*)",
+        ["Już sprawdzam, daj mi chwilę..."]
+
+    ],
     [
         r"witaj|cześć|hej",
         ["Cześć!"]
@@ -16,7 +26,7 @@ pairs = [
     ],
     [
         r"co możesz zrobić",
-        ["Jestem prostym chatbotem i mogę odpowiadać na twoje pytania. Spróbuj zapytać o coś!"]
+        ["Jestem prostym chatbotem i mogę pomóc Ci znaleźć . Spróbuj zapytać o coś!"]
     ],
     [
         r"zamówienie|kupić|produkt",
@@ -34,6 +44,19 @@ pairs = [
 
 # Ustawienia rozpoznawania
 def chatbot():
-    print("Witaj! Jestem twój chatbot. Pytaj mnie o cokolwiek, a postaram się odpowiedzieć.")
+    user_input = ""  # Inicjalizacja zmiennej do przechowywania wpisanego słowa
+    print("Witaj! Jestem twój chatbot. Jaki produkt mam dla Ciebie sprawdzić?")
     chat = Chat(pairs, reflections)
+
     chat.converse()
+
+    while True:
+        user_message = input("> ")
+        user_input_match = reflections.match(r"Sprawdź (.*)", user_message)
+        if user_input_match:
+            print("ok")
+            user_input = user_input_match.group(1)
+            URL = initRequest(user_input)
+            find = Find(URL)
+            # LowestPrice(tab)
+            BestRated(find)
--- main.py
+++ main.py
@@ -1,17 +1,7 @@
 import Responses
 from Responses import chatbot
-import CheckOpinion
-from CheckOpinion import initRequest
-from CheckOpinion import LowestPrice
-from CheckOpinion import Find
-from CheckOpinion import BestRated
 
 
 if __name__ == "__main__":
 
-    #chatbot()
-    item = "lalka"
-    URL = initRequest(item)
-    tab = Find(URL)
-    #LowestPrice(tab)
-    BestRated(tab)
+    chatbot()
